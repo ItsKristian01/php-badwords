f7d936e2b88aa343c5f158a7f083b097af476780
ItsKristian01 <kristianhana11@gmail.com>
2023-12-11 15:56:59 +0100

    add: froms and new file

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
+
 <body>
-    
+    <h1>Scrivi una parola</h1>
+    <form action="censure.php">
+        <label for=""></label>
+        <input type="text" id="" name="">
+
+        <button type="submit">Invia</button>
+
+        <h1>Scrivi una parola da censurare</h1>
+        <form action="censure.php">
+            <label for=""></label>
+            <input type="text" id="" name="">
+
+            <button type="submit">Invia</button>
+        </form>
 </body>
+
 </html>
